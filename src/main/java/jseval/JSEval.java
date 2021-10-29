3eb4bae8423095c856b300bf8cf97e2f0ab2b5e6
Valera <76255437+xzxADIxzx@users.noreply.github.com>
2021-10-29 11:58:35 +0300

    ...

--- src/main/java/jseval/JSEval.java
+++ src/main/java/jseval/JSEval.java
@@ -1,6 +1,7 @@
 package jseval;
 
 import arc.util.*;
+import arc.struct.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 
@@ -9,12 +10,28 @@ import static mindustry.Vars.*;
 @SuppressWarnings("unused")
 public class JSEval extends Plugin {
 
+    private Seq<Player> excep = new Seq<>();
+
     @Override
     public void registerClientCommands(CommandHandler handler) {
         handler.<Player>register("js", "<code...>", "Execute JavaScript code.", (args, player) -> {
             if (player.admin) {
-                 String output = mods.getScripts().runConsole(args[0]);
-                 player.sendMessage("> " + (isError(output) ? "[#ff341c]" + output : output));
+                String output = mods.getScripts().runConsole(args[0]);
+                if (!isExcep(player)) player.sendMessage("> " + (isError(output) ? "[#ff341c]" + output : output));
+            } else {
+                player.sendMessage("[scarlet]You must be admin to use this command.");
+            }
+        });
+
+        handler.<Player>register("return", "Toggle return from /js command", (args, player) -> {
+            if (player.admin) {
+                if (isExcep(player)){
+                    player.sendMessage("[green]Return enabled"));
+                    excep.remove(player);
+                } else {
+                    player.sendMessage("[scarlet]Return disabled"));
+                    excep.add(player);
+                }
             } else {
                 player.sendMessage("[scarlet]You must be admin to use this command.");
             }
@@ -29,5 +46,9 @@ public class JSEval extends Plugin {
         } catch (Throwable e) {
             return false;
         }
+    }
+
+    private boolean isExcep(Player player){
+        return excep.contains(player);
     }
 }
