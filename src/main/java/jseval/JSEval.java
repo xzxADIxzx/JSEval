7e5727f5091901ded191be8b8eec5d79f26d55ba
xzxADIxzx <valerazhuk20122006@gmail.com>
2022-07-31 20:19:49 +0300

    ...

--- src/main/java/jseval/JSEval.java
+++ src/main/java/jseval/JSEval.java
@@ -1,40 +1,35 @@
 package jseval;
 
-import arc.util.*;
-import arc.struct.*;
-import mindustry.gen.*;
-import mindustry.mod.*;
+import arc.struct.Seq;
+import arc.util.CommandHandler;
+import mindustry.gen.Player;
+import mindustry.mod.Plugin;
 
 import static mindustry.Vars.*;
 
-@SuppressWarnings("unused")
 public class JSEval extends Plugin {
 
-    private Seq<Player> excep = new Seq<>();
+    public Seq<String> except = new Seq<>();
 
     @Override
     public void registerClientCommands(CommandHandler handler) {
         handler.<Player>register("js", "<code...>", "Execute JavaScript code.", (args, player) -> {
             if (player.admin) {
                 String output = mods.getScripts().runConsole(args[0]);
-                if (!isExcep(player)) player.sendMessage("> " + (isError(output) ? "[#ff341c]" + output : output));
-            } else {
-                player.sendMessage("[scarlet]You must be admin to use this command.");
-            }
+                if (!isExcepted(player)) player.sendMessage("> " + (isError(output) ? "[#ff341c]" + output : output));
+            } else player.sendMessage("[scarlet]You must be admin to use this command.");
         });
 
-        handler.<Player>register("return", "Toggle return from /js command", (args, player) -> {
+        handler.<Player>register("return", "Toggle return from /js command.", (args, player) -> {
             if (player.admin) {
-                if (isExcep(player)){
-                    player.sendMessage("[green]Return enabled");
-                    excep.remove(player);
+                if (isExcepted(player)) {
+                    player.sendMessage("[green]Return enabled.");
+                    except.remove(player.uuid());
                 } else {
-                    player.sendMessage("[scarlet]Return disabled");
-                    excep.add(player);
+                    player.sendMessage("[scarlet]Return disabled.");
+                    except.add(player.uuid());
                 }
-            } else {
-                player.sendMessage("[scarlet]You must be admin to use this command.");
-            }
+            } else player.sendMessage("[scarlet]You must be admin to use this command.");
         });
     }
 
@@ -48,7 +43,7 @@ public class JSEval extends Plugin {
         }
     }
 
-    private boolean isExcep(Player player){
-        return excep.contains(player);
+    private boolean isExcepted(Player player) {
+        return except.contains(player.uuid());
     }
 }
